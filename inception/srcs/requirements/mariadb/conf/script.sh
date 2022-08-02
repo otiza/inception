ce61ab4a5bec62f00673f3b2570a9b4db7e0326d
otiza <93729229+otiza@users.noreply.github.com>
2022-08-02 14:40:58 +0100

    Add files via upload
    d

---
+++ inception/srcs/requirements/mariadb/conf/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
+    service mysql start
+    sleep 5
+    mysql -u root -e  "CREATE DATABASE IF NOT EXISTS wordpress; flush privileges;"
+    mysql -u root -e "use wordpress; grant all privileges on *.* to '${USER}'@'localhost' IDENTIFIED BY '${USER_P}'; flush privileges;"      
+    mysql -u root -e "use wordpress; grant all privileges on *.* to '${USER}'@'%.%.%.%' IDENTIFIED BY '${USER_P}'; flush privileges;"
+    #mysql -u root wordpress < wordpress.sql
+    mysql -u root -e  "CREATE USER '${SECOND_USER}'@'%.%.%.%' IDENTIFIED BY '${SECOND_PASSWORD}'; flush privileges;"
+    mysql -u root -e "use wordpress; grant all privileges on wordpress.* to '${SECOND_USER}'@'%.%.%.%' IDENTIFIED BY '${SECOND_PASSWORD}'; flush privileges;"
+    #mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${USER_P}'; flush privileges;"
+    #mysql -u ${USER} -p ${USER_P} -e "DELETE FROM mysql.user WHERE User='root'; flush privileges;"
+fi
+
+tail -f
